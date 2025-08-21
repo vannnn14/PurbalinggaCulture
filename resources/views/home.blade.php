18d0f69d5b7773d8c3da69c5ce57f444e69d65e1
vannnn14 <irvanml474@gmail.com>
2025-08-21 22:05:59 +0700

    First commit proyek Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Purba Culture</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <!-- Navbar Start -->
+   @include('navbar')
+    <!-- Navbar End -->
+     <h1> Anjay</h1>
+</body>
+</html>
---
+++ resources/views/sejarah.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Purba Culture</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <!-- Navbar Start -->
+   @include('navbar')
+    <!-- Navbar End -->
+     <section class="max-w-6xl mx-auto p-6">
+     <div class="bg-[#F8EED6] p-6 rounded-lg shadow">
+        <h2 class="text-2xl font-bold mb-4">Tentang Sejarah Purbalingga</h2>
+        <p class="mb-2">
+        Sebuah nama yang pasti tidak akan tertinggal ketika membicarakan sejarah Purbalingga adalah Kyai Arsantaka, seorang tokoh yang menurut sejarah menurunkan tokoh-tokoh Bupati Purbalingga.Kyai Arsantaka yang pada masa mudanya bernama Kyai Arsakusuma adalah putra dari Bupati Onje II.    </p>
+        <p>
+        Sebuah nama yang pasti tidak akan tertinggal Ketika membicarakan sejarah Purbalingga adalah Kyai Arsantaka, seorang tokoh yang menurut sejarah menurunkan tokoh-tokoh Bupati Purbaligga. Kyai Atsantaka yang pada masa mudanya Bernama Kyai Arsakusuma adalah putra dari Bupati Onje II.
+        </p>
+    </div>
+     </section>
+</body>
+</html>
